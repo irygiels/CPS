e4edd23f5272a3a74289f9c66fbc5f48926f32ff
irygiels <tojestpozadanylogin@gmail.com>
2017-05-10 20:07:26 +0200

    comments in eng

--- lpc10.m
+++ lpc10.m
@@ -1,21 +1,33 @@
-clear all; clf;
+close all;
+clear;
+clf;
 
 [x,fpr]=audioread('mowa1.wav');	
-plot(x); title('sygnal mowy'); pause
-soundsc(x,fpr);	
 
-N=length(x);	
-Mlen=240;		 
-Mstep=180;		 
-Np=10;			 
-gdzie=Mstep+1;	 
+org = x;
+n = 1.1e4: 1.2e4; 
+d = 1e4:1.1e4; 
+
+%wyswietl na wykresie
+hold on;
+plot(x); title('speech signal');
+plot(x(n), 'r');
+plot(x(d), 'g');
+hold off;
+% pause;
+
+N=length(x);	 %signal length
+Mlen=240;		 %hamming window length
+Mstep=180;		 %time shift
+Np=10;			 %prediction filter
+pos=Mstep+1;	 %initial position
 
 lpc=[];								 
 s=[];								
 ss=[];								  
 bs=zeros(1,Np);
-Nramek=floor((N-Mlen)/Mstep+1);	
-for  nr = 1 : Nramek
+Nframes=floor((N-Mlen)/Mstep+1);	
+for  nr = 1 : Nframes
     
     n = 1+(nr-1)*Mstep : Mlen + (nr-1)*Mstep;
     bx = x(n);
@@ -34,19 +46,21 @@ for  nr = 1 : Nramek
         R(m,1:Np)=[r(m:-1:2) r(1:Np-(m-1))];			
     end
     a=-inv(R)*rr;											
-    wzm=r(1)+r(2:Np+1)*a;									
-    if (T~=0) gdzie=gdzie-Mstep; end					
+    amplifier=r(1)+r(2:Np+1)*a;									
+    if (T~=0) pos=pos-Mstep; end					
     for n=1:Mstep
         if( T==0)
-            pob=2*(rand(1,1)-0.5); gdzie=(3/2)*Mstep+1;			
+            pob=2*(rand(1,1)-0.5); pos=(3/2)*Mstep+1;			
         else
-            if (n==gdzie) pob=1; gdzie=gdzie+T;	   
+            if (n==pos) pob=1; pos=pos+T;	   
             else pob=0; end
         end
-        ss(n)=wzm*pob-bs*a;		
+        ss(n)=amplifier*pob-bs*a;		
         bs=[ss(n) bs(1:Np-1) ];	
     end
     s = [s ss];						
 end
-plot(s); title('mowa zsyntezowana'); pause
-soundsc(s, fpr)
+plot(s); title('speech - synthesis'); pause
+soundsc(s, fpr); pause
+soundsc(x,fpr);	
+
