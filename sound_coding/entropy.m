1965a416ae4fbf26cb2c34c7440b8c16de129e53
irygiels <tojestpozadanylogin@gmail.com>
2017-05-19 06:54:38 +0200

    entropy comments added

--- sound_coding/entropy.m
+++ sound_coding/entropy.m
@@ -1,27 +1,32 @@
-function y = huffman( x )
-%HUFFMAN Summary of this function goes here
-%   Detailed explanation goes here
-
+function y = entropy( x )
 len=length(x);
-%liczba roznych symboli
+%number of different symbols
 diff = numel(unique(x))
-sym = zeros(diff, 2); %tablica na symbole
+sym = zeros(diff, 2); %an array for storing symbols
 x=sort(x);
-pointer=1; %wskazuje w jakim jestem rzedzie
-sym(pointer,:)=[x(1),1]; %wpisuje pierwszy element do tablicy
-%prawdopodobienstwa
+pointer=1; %which row is now considered
+sym(pointer,:)=[x(1),1]; %the first column is x
 for i=2:len
-    if x(i)-x(i-1)~=0 %jesli nowy element
+    if x(i)-x(i-1)~=0 %move pointer and write the new element
         pointer=pointer+1;
-        sym(pointer,:) = [x(i),1]; %wpisz nowy element z licznikiem 1
+        sym(pointer,:) = [x(i),1];
     else 
-        sym(pointer,2) = sym(pointer,2)+1; %zwieksz o 1 liczbe wystapien
+        sym(pointer,2) = sym(pointer,2)+1; %increase the counter
     end
 end
 
-%dane zachowywane w formacie: symbol, prawdopodobienstwo
+%data stored in format: x(i), probability(x(i))
 sym(:,2)=sym(:,2)./len;
-sym=sortrows(sym,2);  
 
+%sort (ascending probability)
+sym=sortrows(sym,2);  
+sym
+
+%entropy
+H=0;
+for i=1:diff
+    H=H-sym(i,2)*log2(sym(i,2));
 end
 
+H
+end
